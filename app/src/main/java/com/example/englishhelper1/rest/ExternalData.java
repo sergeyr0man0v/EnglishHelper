097ba1ca54a10a64622da25de7c4cc2303714478
sergeyr0man0v <serov.serzh2017@yandex.ru>
2022-05-27 10:06:51 +0300

    Connection

---
+++ app/src/main/java/com/example/englishhelper1/rest/ExternalData.java
@@ -0,0 +1,13 @@
+package com.example.englishhelper1.rest;
+
+import com.example.englishhelper1.models.Module;
+import com.example.englishhelper1.models.Section;
+import com.example.englishhelper1.models.Word;
+
+import java.util.ArrayList;
+
+public class ExternalData {
+    public static ArrayList<Module> modules = new ArrayList<>();
+    public static ArrayList<Section> sections = new ArrayList<>();
+    public static ArrayList<Word> words = new ArrayList<>();
+}
